ef86d02e19b4be454b7d1d39737695fd4161eede
MaciejuZ <MaciejuZ@users.noreply.github.com>
2023-12-17 13:00:38 +0100

    Changed name and msg

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,8 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
-msg = "Hello World!"
+moje_imie = "Maciej"
+msg = "Wants to wish you a Merry Christmas"
 
 @app.route('/')
 def index():
